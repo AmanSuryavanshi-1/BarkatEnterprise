11bf8a4219c9883f7388c1a66577ab10a4bc2a91
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2025-01-09 13:01:16 +0530

    added catalogues again & removed git repo from main deployment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,8 @@ import {
   FaMapMarkerAlt,
   FaInfoCircle,
   FaPhoneAlt,
-  FaAngleUp
+  FaAngleUp,
+  FaBook
 } from 'react-icons/fa'
 
 export default function Header() {
@@ -28,7 +29,7 @@ export default function Header() {
   const menuItems = [
     { name: 'Home', link: '/', icon: FaHome },
     { name: 'Products', link: '/products', icon: FaBox },
-    // { name: 'Catalogues',link: '/catalogues', icon: FaBook },
+    { name: 'Catalogues',link: '/catalogues', icon: FaBook },
     { name: 'Where to Buy', link: '/where-to-buy', icon: FaMapMarkerAlt },
     { name: 'About Us', link: '/about', icon: FaInfoCircle },
     { name: 'Contact Us', link: '/contact', icon: FaPhoneAlt },
